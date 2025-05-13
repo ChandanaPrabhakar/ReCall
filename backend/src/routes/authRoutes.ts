a2d19f8891d32c7fecf2eb9d8456b999ce6f3056
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-05-14 04:08:43 +0530

    created api to fetch all notes of user

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import { signupController, loginController } from '../controllers/authControllers';
-import { addNoteController, editNoteController } from "../controllers/noteControllers";
+import { addNoteController, editNoteController, getAllNotesController } from "../controllers/noteControllers";
 import { authenticationToken } from "../utils/utilities";
 
 const router = Router();
@@ -9,5 +9,6 @@ router.post('/signup', signupController);
 router.post('/login', loginController);
 router.post('/add-note' , authenticationToken ,addNoteController);
 router.put('/edit-note/noteId/:noteId' , authenticationToken ,editNoteController);
+router.get('/get-all-notes' , authenticationToken ,getAllNotesController);
 
 export default router;
