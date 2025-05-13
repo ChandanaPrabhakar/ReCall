03641423e3fe4df4341a6cf686a57af343ca6329
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-05-14 03:33:32 +0530

    created edit note by Id api

--- backend/src/controllers/noteControllers.ts
+++ backend/src/controllers/noteControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import jwt from 'jsonwebtoken';
-import { addNoteService } from "../services/noteServices";
+import { addNoteService, editNoteService } from "../services/noteServices";
 
 export const addNoteController = async(req: Request, res: Response) => {
     const {title, content, tags} = req.body;
@@ -17,5 +17,26 @@ export const addNoteController = async(req: Request, res: Response) => {
     }catch(error){
         console.error("Failed adding note", error);
         res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const editNoteController = async(req: Request, res: Response) => {
+    const {noteId} = req.params;
+    const {title, content, tags, isPinned} = req.body;
+    const {user} = req.user as jwt.JwtPayload;
+
+    if(!title && !content && !tags){
+        return res.status(400).json({message: "No changes provided"});
+    }
+
+    try{
+        const data = await editNoteService(noteId, title, content, tags, isPinned, user);
+        if(!data?.success){
+            return res.status(404).json({message: data?.message});
+        }
+        res.status(200).json({data, message: data?.message});
+    }catch(error){
+        console.error("Failed editing note", error);
+        res.status(500).json({message: 'Internal server error'});
     }
 }
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import { signupController, loginController } from '../controllers/authControllers';
-import { addNoteController } from "../controllers/noteControllers";
+import { addNoteController, editNoteController } from "../controllers/noteControllers";
 import { authenticationToken } from "../utils/utilities";
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 router.post('/signup', signupController);
 router.post('/login', loginController);
 router.post('/add-note' , authenticationToken ,addNoteController);
+router.put('/edit-note/noteId/:noteId' , authenticationToken ,editNoteController);
 
 export default router;
