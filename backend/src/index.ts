c1740e43b19385ec432081f8228ea64111745988
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-05-14 19:27:32 +0530

    Minor refactoring of jwt paylaod of users to destruct _id

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import {databaseConnection} from './config/db';
+import { databaseConnection } from './config/db';
 import authRouters from './routes/authRoutes';
 
 dotenv.config();
@@ -10,12 +10,12 @@ databaseConnection();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:'*'
+    origin: '*'
 }));
 app.use('/api/auth', authRouters);
 
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server listening in ${PORT}.....`)
 })
