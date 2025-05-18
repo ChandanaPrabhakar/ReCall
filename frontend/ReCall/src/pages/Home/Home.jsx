c9869f07947c287eb89090b8cfe1f05929e9c03a
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-05-18 18:03:00 +0530

    Made modal component responsive

--- frontend/ReCall/src/pages/Home/Home.jsx
+++ frontend/ReCall/src/pages/Home/Home.jsx
@@ -203,7 +203,7 @@ const Home = () => {
           },
         }}
         contentLabel=""
-        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-hidden"
+        className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[40%] max-h-[90vh bg-white rounded-md mx-auto mt-14 p-5 overflow-auto"
       >
         <AddEditNotes
           type={openAddEditModal.type}
